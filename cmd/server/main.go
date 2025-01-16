9fdf527438abee8b4323d5247451c8eebd60703a
Longs Pemun Gotar <developerlongs@gmail.com>
2025-01-16 09:01:38 +0100

    feat: add entrypoint for starting server

---
+++ cmd/server/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/devlongs/evm-disassembler/internal/api"
+	"github.com/devlongs/evm-disassembler/internal/config"
+	"github.com/devlongs/evm-disassembler/internal/logger"
+	"go.uber.org/zap"
+)
+
+func main() {
+	cfg, err := config.LoadConfig("configs/config.yaml")
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
+
+	logg, err := logger.NewLogger(cfg.Logger.Level)
+	if err != nil {
+		log.Fatalf("failed to initialize logger: %v", err)
+	}
+	defer logg.Sync()
+
+	r := api.SetupRouter(logg)
+
+	addr := fmt.Sprintf(":%d", cfg.Server.Port)
+	logg.Info("Starting EVM Disassembler server",
+		zap.String("address", addr),
+		zap.String("log_level", cfg.Logger.Level),
+	)
+
+	if err := r.Run(addr); err != nil {
+		logg.Fatal("Failed to run server", zap.Error(err))
+	}
+}
